0e64ce0b5389a9a17cec33bc96956fd2437a2f1c
unknown <samueldecandioscheffer@gmail.com>
2024-01-20 11:51:46 -0300

    Feature: New Class 'estoque'

---
+++ Estoque.cs
@@ -0,0 +1,22 @@
+using System;
+namespace Estoque
+{
+
+    // int string bool char double 
+    class Produto
+    {
+        public double? Quantidade, Id, Valor;
+        public Produto(int quantidade, int id, int valor)
+        {
+            Quantidade = quantidade;
+            Id = id;
+            Valor = valor;
+        }
+
+        class Adicionar
+        {
+            
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Data.Common;
+using Estoque;
 
 namespace Csharp
 {
@@ -9,66 +10,67 @@ namespace Csharp
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Clear();
-            // id, valor, quantidade
-            double[] Óleo = {2, 5, 3};
-            double[] Farinha = {1, 10, 3};
-            double[] DuziaDeOvos = {3, 20, 3};
-            double[] Carne = {0, 15, 3};
+        {                        // quantidade, id, valor
+            Produto Farinha = new Produto(3, 1, 10);
+            Produto Oleo = new Produto(3, 2, 5);
+            Produto DuziaDeOvos = new Produto(3, 3, 20);
+            Produto Carne = new Produto(3, 4, 15);
 
             while (true)
             {
-                Console.WriteLine("Oque deseja fazer? \n\n[1] adicionar \n[2] atender \n[3] estoque");
-                string tarefa = Console.ReadLine();
+                Console.WriteLine("Oque deseja fazer? \n\n[1] adicionar \n[2] atender \n[3] estoque\n");
+                string? tarefa = Console.ReadLine();
                 Console.Clear();
 
                 if (tarefa == "adicionar" || tarefa == "1")
                 {
-                    Console.WriteLine("Qual produto deseja adicionar? \n\n[1] Óleo \n[2] Farinha \n[3] Duzia de ovos \n[4] Carne\n\n");
-                    string adicionar = Console.ReadLine();
-
+                    Console.WriteLine("Qual produto deseja adicionar? \n\n[1] Óleo \n[2] Farinha \n[3] Duzia de ovos \n[4] Carne\n");
+                    string? adicionar = Console.ReadLine();
 
                     if (adicionar == "Óleo" || adicionar == "1" || adicionar == "oleo")
                     {
                         Console.Write("Quantidade: ");
-                        Óleo[2] += AlterarQntdProdutos(1, int.Parse(Console.ReadLine()));
-                        Console.WriteLine("estoque: " + Óleo[2]);
+                        Oleo.Quantidade += AlterarQntdProdutos(1, Console.ReadLine());
+
+                        // Console.WriteLine("estoque: " + Óleo[2]);
                     }
 
                     else if (adicionar == "Farinha" || adicionar == "2")
                     {
                         Console.Write("Quantidade: ");
-                        Farinha[2] += AlterarQntdProdutos(1, int.Parse(Console.ReadLine()));
+                        Farinha.Quantidade += AlterarQntdProdutos(1, Console.ReadLine());
+                        // Console.WriteLine("estoque: " + Farinha[2]);
                     }
 
                     else if (adicionar == "Duzia de ovos" || adicionar == "3" || adicionar == "Ovos")
                     {
                         Console.Write("Quantidade: ");
-                        DuziaDeOvos[2] += AlterarQntdProdutos(1, int.Parse(Console.ReadLine()));
+                        DuziaDeOvos.Quantidade += AlterarQntdProdutos(1, Console.ReadLine());
+                        // Console.WriteLine("estoque: " + DuziaDeOvos[2]);
                     }
 
                     else if (adicionar == "Carne" || adicionar == "4")
                     {
                         Console.Write("Quantidade: ");
-                        Carne[2] += AlterarQntdProdutos(1, int.Parse(Console.ReadLine()));
+                        Carne.Quantidade += AlterarQntdProdutos(1, Console.ReadLine());
+                        // Console.WriteLine("estoque: " + Carne[2]);
                     }
                 }
                 if (tarefa == "atender" || tarefa == "2")
                 {
                     Console.Write("Qual receita deseja fazer? \n[1] Lasanha \n[2] Pastél assado \n[3] Bife acebolado \n[4] Escondidinho de carne\n\n");
-                    string atender = Console.ReadLine();
+                    string? atender = Console.ReadLine();
                     Console.Write("\n");
 
                     if (atender == "1" || atender == "lasanha")
                     {
-                        if (Óleo[2] >= 1 && Farinha[2] >= 1 && DuziaDeOvos[2] >= 1 && Carne[2] >= 1)
+                        if (Oleo.Quantidade >= 1 && Farinha.Quantidade >= 1 && DuziaDeOvos.Quantidade >= 1 && Carne.Quantidade >= 1)
                         {
                             Console.WriteLine("O garfield iria amar\n");
-                            Óleo[2] -= 1;
-                            Farinha[2] -= 1;
-                            DuziaDeOvos[2] -= 1;
-                            Carne[2] -= 1;
+                            Oleo.Quantidade -= 1;
+                            Farinha.Quantidade -= 1;
+                            DuziaDeOvos.Quantidade -= 1;
+                            Carne.Quantidade -= 1;
                         }
                         else
                         {
@@ -78,12 +80,12 @@ namespace Csharp
 
                     if (atender == "2" || atender == "Pastél" || atender == "Pastél assado")
                     {
-                        if (Farinha[2] >= 1 && DuziaDeOvos[2] >= 0.5 && Carne[2] >= 1)
+                        if (Farinha.Quantidade >= 1 && DuziaDeOvos.Quantidade >= 0.5 && Carne.Quantidade >= 1)
                         {
                             Console.WriteLine("Pastél feito\n");
-                            Farinha[2] -= 1;
-                            DuziaDeOvos[2] -= 0.5;
-                            Carne[2] -= 1;
+                            Farinha.Quantidade -= 1;
+                            DuziaDeOvos.Quantidade -= 0.5;
+                            Carne.Quantidade -= 1;
                         }
                         else
                         {
@@ -93,11 +95,11 @@ namespace Csharp
 
                     if (atender == "3" || atender == "Bife acebolado")
                     {
-                        if (Óleo[2] >= 1 && Carne[2] >= 1)
+                        if (Oleo.Quantidade >= 1 && Carne.Quantidade >= 1)
                         {
                             Console.WriteLine("Fazendo o bife\n");
-                            Óleo[2] -= 1;
-                            Carne[2] -= 1;
+                            Oleo.Quantidade -= 1;
+                            Carne.Quantidade -= 1;
                         }
                         else
                         {
@@ -107,11 +109,11 @@ namespace Csharp
 
                     if (atender == "4" || atender == "Escondidinho de carne")
                     {
-                        if (Farinha[2] >= 1 && Carne[2] >= 1)
+                        if (Farinha.Quantidade >= 1 && Carne.Quantidade >= 1)
                         {
                             Console.WriteLine("fazendo escondidinho\n");
-                            Farinha[2] -= 1;
-                            Carne[2] -= 1;
+                            Farinha.Quantidade -= 1;
+                            Carne.Quantidade -= 1;
                         }
                         else
                         {
@@ -120,32 +122,38 @@ namespace Csharp
                     }
 
                 }
-                
-                    if (tarefa == "estoque" || tarefa == "3")
-                    {
-                        Console.WriteLine("Carne \nQuantidade: " + Carne[2]);
-                        Console.WriteLine("Valor: " + Carne[1]);
-                        Console.WriteLine("id: " + Carne[0]);
-                        
-                        Console.WriteLine("\nFarinha \nQuantidade: " + Farinha[2]);
-                        Console.WriteLine("Valor: " + Farinha[1]);
-                        Console.WriteLine("id: " + Farinha[0]);
-                        
-                        Console.WriteLine("\nÓleo \nQuantidade: " + Óleo[2]);
-                        Console.WriteLine("Valor: " + Óleo[1]);
-                        Console.WriteLine("id: " + Óleo[0]);
-                        
-                        Console.WriteLine("\nDuzia(s) de ovos \nQuantidade: " + DuziaDeOvos[2]);
-                        Console.WriteLine("Valor: " + DuziaDeOvos[1]);
-                        Console.WriteLine("id: " + DuziaDeOvos[0]);
-                        Console.WriteLine("\n");
+                if (tarefa == "estoque" || tarefa == "3")
+                {
+                    Console.WriteLine("Carne \nQuantidade: " + Carne.Quantidade);
+                    Console.WriteLine("Valor: " + Carne.Valor);
+                    Console.WriteLine("id: " + Carne.Id);
 
-                    }
+                    Console.WriteLine("\nFarinha \nQuantidade: " + Farinha.Quantidade);
+                    Console.WriteLine("Valor: " + Farinha.Valor);
+                    Console.WriteLine("id: " + Farinha.Id);
+
+                    Console.WriteLine("\nÓleo \nQuantidade: " + Oleo.Quantidade);
+                    Console.WriteLine("Valor: " + Oleo.Valor);
+                    Console.WriteLine("id: " + Oleo.Id);
+
+                    Console.WriteLine("\nDuzia(s) de ovos \nQuantidade: " + DuziaDeOvos.Quantidade);
+                    Console.WriteLine("Valor: " + DuziaDeOvos.Valor);
+                    Console.WriteLine("id: " + DuziaDeOvos.Id);
+                    Console.WriteLine("\n");
+
+                }
             }
             //função pra alterar a quantidade de produtos (se o nome não for indicativo o bastante)
-            static int AlterarQntdProdutos(int ação, int QntdAlterar)
+            static int AlterarQntdProdutos(int ação, string QntdAlterar)
             {
-                return ação * QntdAlterar;
+                if (QntdAlterar == "")
+                {
+                    return 0;
+                }
+                else
+                {
+                    return ação * int.Parse(QntdAlterar);
+                }
             }
         }
     }
